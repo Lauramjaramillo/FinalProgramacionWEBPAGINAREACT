38d53d3582ed45b5a6e2f52edcb91953fc4c99cd
Laura Jaramillo <lauris9449@gmail.comm>
2023-10-20 15:38:10 -0500

    V.1.5

--- src/Componentes/home/Home.js
+++ src/Componentes/home/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
+import React, { useState, useEffect } from "react";
 
 function NavHome(props) {
   return (
@@ -55,6 +55,72 @@ function NavHome(props) {
   );
 }
 
+function Card(props) {
+  const [pokemon, setPokemon] = useState();
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPokemon(data)
+      });
+  }, []);
+
+  return pokemon ? (
+    <div className="" style={{ width: "310px", border: "1px solid black" }} key={pokemon.id}>
+      <img src={pokemon.sprites.front_default} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{pokemon.name}</h5>
+        <p className="card-text">
+
+        </p>
+        <a href="#" className="btn btn-dark btn-sm">
+          Agregar al carrito
+        </a>
+      </div>
+    </div>
+  ) : null
+}
+
+function App() {
+  const [data, setData] = useState([]);
+
+
+
+  useEffect(() => {
+    //Fetch para extraer datos del pokemon
+
+    // if (!loaded) {
+
+    // pokemons.forEach((pokemon) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // setData((arrayViejo) => [
+        //   ...arrayViejo,data
+        //   // <Card key={data.id} title={data.name} img={data.sprites.front_default}></Card>,
+        // ]);
+        setData(data)
+      });
+  }, []);
+
+  // setLoaded(true);
+  // }
+  // }, []);
+
+  return data?.results ? (
+    <div className="App">
+
+      {/* <div className="container d-flex justify-content-around gap-4">{data}</div> */}
+      {<div className="container d-grid justify-content-around gap-4" style={{ width: "800px", gridTemplateColumns: "1fr 1fr 1fr" }}>
+        {data.results.map(item => <Card key={item.id} name={item.name} />)}
+      </div>}
+
+    </div>
+  ) : null
+}
+
 
 
 
@@ -65,8 +131,12 @@ function Home(props) {
       <div className="titulos">
         <h1>ISIS SHOP</h1>
       </div>
+
+      <App></App>
+
+
     </>
   );
 }
 
-export {Home}
+export { Home }
